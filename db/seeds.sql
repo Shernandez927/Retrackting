26a91c389ebe5472e76fb56fd7a64d71e3de20a8
Monet <shelbyhernandez9876@yahoo.com>
2022-10-24 17:51:30 -0500

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (department_name)
+VALUES  ("Engineering"),
+        ("Finance"),
+        ("Legal"),
+        ("Sales");
+
+INSERT INTO roles (title, salary)
+VALUES  ("Sales Lead", 10000),
+        ("Salesperson", 80000),
+        ("Lead Engineer", 150000),
+        ("Software Engineer", 120000),
+        ("Account Manager", 160000),
+        ("Accountant", 125000),
+        ("Legal Team Lead", 250000),
+        ("Lawyer", 190000);
+
+INSERT INTO employees (first_name, last_name)
+VALUES  ("Jimmy", "McGill"),
+        ("Mike", "Ehermantraut"),
+        ("Kim", "Wexler"),
+        ("Howard", "Hamlin"),
+        ("Nacho", "Vargo"),
+        ("Chuck", "McGill"),
+        ("Gustavo", "Fring"),
+        ("Lalo", "Salamanca");
--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
 require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL Username
+      user: 'root',
+      password: '',
+      database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
+  );
