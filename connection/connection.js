9383db747b7397c6acb59490888ef8e65a678859
Monet <shelbyhernandez9876@yahoo.com>
2022-10-25 22:30:34 -0500

    edit

--- connection/connection.js
+++ connection/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection(
     {
       host: "localhost",
       user: "root",
-      password: "Splicer4851107#", 
+      password: "", 
       database: "company_db"
     });
 
